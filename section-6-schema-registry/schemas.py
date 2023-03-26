6acc4664dc6c465cb3145729d792800fcb8fc9fa
hohodsj <hohodsj@gmail.com>
2023-03-26 14:44:14 -0400

    schema registry

---
+++ section-6-schema-registry/schemas.py
@@ -0,0 +1,41 @@
+person_value_v1="""
+    {
+        "namespace":"com.hoho.avrodomainevents",
+        "name": "Person",
+        "type": "record",
+        "fields": [
+            {
+                "name":"name",
+                "type":"string"
+            },
+            {
+                "name":"title",
+                "type":"string"
+            }
+        ]
+    }
+"""
+
+person_value_v2="""
+    {
+        "namespace":"com.hoho.avrodomainevents",
+        "name": "Person",
+        "type": "record",
+        "fields": [
+            {
+                "name":"first_name",
+                "type":["null","string"],
+                "default": null
+            },
+            {
+                "name":"last_name",
+                "type":["null","string"],
+                "default": null
+            },
+            {
+                "name":"title",
+                "type":"string"
+            }
+        ]
+    }
+"""
